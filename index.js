60d03cf8dd3a7542b3e8c4c8fc58bf4ab5d5736c
Raizy Einhorn <raizyeinhorn@gmail.com>
2020-03-31 15:38:49 -0400

    linked secure jquery for github deployment

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 //responsible to toggle the hamburger menu
 
 function toggleMenu(){
-    console.log('`toggleMenu` ran')
     $('nav').on('click', '.ham-button', function(){
         $(this).closest("nav").find('ul').toggleClass('hide-nav-link');
         });
